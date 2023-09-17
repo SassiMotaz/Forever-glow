569b31e0cc9c00862ae3d69f12162ed8755c3d30
Motaz <sassimotaze99@gmail.com>
2023-09-17 13:00:23 +0000

    Deploy

--- .sanity/runtime/app.js
+++ .sanity/runtime/app.js
@@ -1,11 +1,3 @@
-
-// This file is auto-generated on 'sanity dev'
-// Modifications to this file is automatically discarded
-import {renderStudio} from "sanity"
-import studioConfig from "..\\..\\sanity.config.ts"
-
-renderStudio(
-  document.getElementById("sanity"),
-  studioConfig,
-  {reactStrictMode: false, basePath: "/"}
-)
+version https://git-lfs.github.com/spec/v1
+oid sha256:b80b5543c457040d5ea50212cef1f125a0e07cf63c999b4a0af4478b1caa6f15
+size 305
