8399336bbb6bea2605801996f670f53e4e311d66
Motaz <sassimotaze99@gmail.com>
2023-09-19 10:22:57 +0000

    mise en page dt

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f73351c4c09dabbf067943f7b89dbbe3a513f38af79538926cc36e01fdc91271
+oid sha256:49231d448c985dd41d7bf20f0657149df0483b84014843c7188cb41f4c167d7b
 size 1598
