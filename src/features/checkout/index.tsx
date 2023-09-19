587e1988197d76e67ad7f1f3d0a8a82a446664c1
Motaz <sassimotaze99@gmail.com>
2023-09-19 10:27:27 +0000

    mise en page checkout

--- src/features/checkout/index.tsx
+++ src/features/checkout/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a87f4ea9f5da738b495dba4b84549760143b18926fc01de2cdcbfc84f2d8995
-size 8323
+oid sha256:74246232cdeee0cfb7f7433fe41722fcdc3b6494818b6940ffd72e5bc395d784
+size 8324
