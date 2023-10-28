9adf4885ca0f6b230afc9db9ca778a28bc3718e9
Motaz <sassimotaze99@gmail.com>
2023-10-28 12:21:42 +0000

    deploy cover image

--- src/features/home/Banner.tsx
+++ src/features/home/Banner.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b170c17e8e1cffbfdfd58f5f5bbdaad76a3738a13acc4d76a3d22622343d7f20
-size 1684
+oid sha256:d4e84335ed02eaed1302c47cec8adfa4d3033db380713374d6e3750ad72fe4fb
+size 1662
