f93f4acb49b5d80e4c515bcd39679dab19d2e006
Motaz <sassimotaze99@gmail.com>
2023-11-18 15:29:03 +0000

    fix swiper1

--- src/features/home/BannerSlider.tsx
+++ src/features/home/BannerSlider.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e0916bfe4f514b4038af2c1362933bd922ef30a53596890a90da1b05bdeaeb23
-size 1737
+oid sha256:72ccef470b8b7b0402eee77aa3372281666e5c8358830989f77715dcaa179966
+size 1758
