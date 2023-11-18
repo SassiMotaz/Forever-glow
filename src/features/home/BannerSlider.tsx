70de0f27a81be70925e7c648c21a7e8eeddeb968
Motaz <sassimotaze99@gmail.com>
2023-11-18 15:14:08 +0000

    deploy images

--- src/features/home/BannerSlider.tsx
+++ src/features/home/BannerSlider.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:770cff8655e98dea3a03b9effadc35f5b137c10df7a0fc8f4dc39f2d7da1cf66
-size 1763
+oid sha256:53e19e29685592c102c1685e494685990e8640bbcf20656b8a7a97b29d9bcb82
+size 2173
